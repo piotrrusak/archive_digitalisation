85c0580180253c1b1c947087bcc1aeab212d3f0d
Piotr Rusak <134606021+piotrrusak@users.noreply.github.com>
2025-08-23 16:14:43 +0200

    ARC-18 ARC-19 ARC-22 ARC-23 ARC-24 ARC-20 ARC-21 ARC-25 Rest complete (#15)
    * rest_initialisation
    
    * model
    
    * repository
    
    * service
    
    * controller
    
    * ci_update
    
    * ci_update_v2
    
    * review_changes_v2
    
    * review_changes_v3
    
    * review_changes_v4

---
+++ backend/src/main/java/edu/bachelor/rest/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.bachelor.rest.repository;
+
+import edu.bachelor.rest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
