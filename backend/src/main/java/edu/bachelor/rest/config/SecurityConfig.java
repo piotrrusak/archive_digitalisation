68a06b05cab006da30407f9450f1f0eb81988b52
piotrrusak <piotrrusak2003@gmail.com>
2025-08-29 20:28:54 +0200

    origins_update

--- backend/src/main/java/edu/bachelor/rest/config/SecurityConfig.java
+++ backend/src/main/java/edu/bachelor/rest/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://twoja-domena.pl"));
+        config.setAllowedOrigins(List.of("http://localhost:5173/", "http://localhost:8000/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setExposedHeaders(List.of("Authorization", "Location"));
