301fc00771720335ee2a248cb257f3a145eea4cf
piotrrusak <piotrrusak2003@gmail.com>
2025-09-03 23:56:46 +0200

    moving_env_to_properties

--- backend/src/main/java/edu/bachelor/rest/config/AuthProps.java
+++ backend/src/main/java/edu/bachelor/rest/config/AuthProps.java
@@ -3,24 +3,16 @@ package edu.bachelor.rest.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "auth")
+@Getter @Setter
 class AuthProps {
     private String baseUrl;
     private String path;
     private int connectTimeoutMs = 1000;
     private int readTimeoutMs = 1500;
     private String bypassToken;
-    
-    public String getBaseUrl() { return baseUrl; }
-    public void setBaseUrl(String baseUrl) { this.baseUrl = baseUrl; }
-    public String getPath() { return path; }
-    public void setPath(String path) { this.path = path; }
-    public int getConnectTimeoutMs() { return connectTimeoutMs; }
-    public void setConnectTimeoutMs(int connectTimeoutMs) { this.connectTimeoutMs = connectTimeoutMs; }
-    public int getReadTimeoutMs() { return readTimeoutMs; }
-    public void setReadTimeoutMs(int readTimeoutMs) { this.readTimeoutMs = readTimeoutMs; }
-    public String getBypassToken() { return bypassToken; }
-    public void setBypassToken(String bypassToken) { this.bypassToken = bypassToken; }
-
 }
