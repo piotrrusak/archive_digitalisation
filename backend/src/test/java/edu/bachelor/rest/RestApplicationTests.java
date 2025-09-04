ada2b02d5cc1c7ecdd25df52080f16c1ba387546
piotrrusak <piotrrusak2003@gmail.com>
2025-09-04 12:37:12 +0200

    backend_separation_from_docker

--- backend/src/test/java/edu/bachelor/rest/RestApplicationTests.java
+++ backend/src/test/java/edu/bachelor/rest/RestApplicationTests.java
@@ -2,7 +2,9 @@ package edu.bachelor.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class RestApplicationTests {
 
