fdfe7777f5eedfed9802bfaf0a242cc2391c0d24
Ernest <ernest.szlamczyk@swmansion.com>
2025-10-26 13:16:18 +0100

    ci fixes

--- auth/config/runtime.exs
+++ auth/config/runtime.exs
@@ -23,6 +23,10 @@ end
 # during deployment do it properly
 config :auth, :jwt_secret, "test"
 
+config :auth, :backend_authorization_token,
+  System.get_env("BACKEND_AUTHORIZATION_TOKEN") ||
+    raise "missing BACKEND_AUTHORIZATION_TOKEN environment variable"
+
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
