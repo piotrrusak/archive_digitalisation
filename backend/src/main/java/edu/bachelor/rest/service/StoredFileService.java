866550c9ae3615252f6b8272af0380162347acb2
piotrrusak <piotrrusak2003@gmail.com>
2025-11-02 19:02:49 +0100

    fix_v2

--- backend/src/main/java/edu/bachelor/rest/service/StoredFileService.java
+++ backend/src/main/java/edu/bachelor/rest/service/StoredFileService.java
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
