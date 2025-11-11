4adddcab23c7e978e9649c38482e19787f6f419d
Piotr Rusak <piotrrusak2003@gmail.com>
2025-11-11 10:56:03 +0100

    fmt

--- backend/src/main/java/edu/bachelor/rest/controller/InformationController.java
+++ backend/src/main/java/edu/bachelor/rest/controller/InformationController.java
@@ -1,24 +1,22 @@
 package edu.bachelor.rest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.bachelor.rest.dto.AvailableModels;
 import edu.bachelor.rest.service.InformationService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/information")
 @RestController
 @RequiredArgsConstructor
 public class InformationController {
-    
-    private InformationService informationService;
 
-    @GetMapping("/available_models")
-    public AvailableModels getAvailableModels(HttpServletRequest request) {
-        return this.informationService.getAvailableModels(request);
-    }
+  private InformationService informationService;
 
+  @GetMapping("/available_models")
+  public AvailableModels getAvailableModels(HttpServletRequest request) {
+    return this.informationService.getAvailableModels(request);
+  }
 }
--- backend/src/main/java/edu/bachelor/rest/service/InformationService.java
+++ backend/src/main/java/edu/bachelor/rest/service/InformationService.java
@@ -1,8 +1,8 @@
 package edu.bachelor.rest.service;
 
+import edu.bachelor.rest.dto.AvailableModels;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,26 +10,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import edu.bachelor.rest.dto.AvailableModels;
-
 @Service
 @RequiredArgsConstructor
 public class InformationService {
 
-    private final WebClient webClient;
-    
-    @Value("${ocr.path}")
-      private String ocrPath;
-    
-    @Transactional
-    public AvailableModels getAvailableModels(HttpServletRequest request) {
-        return this.webClient
-                   .post()
-                   .uri(this.ocrPath)
-                   .header(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION))
-                   .contentType(MediaType.APPLICATION_JSON)
-                   .retrieve()
-                   .bodyToMono(AvailableModels.class)
-                   .block();
-    }
+  private final WebClient webClient;
+
+  @Value("${ocr.path}")
+  private String ocrPath;
+
+  @Transactional
+  public AvailableModels getAvailableModels(HttpServletRequest request) {
+    return this.webClient
+        .post()
+        .uri(this.ocrPath)
+        .header(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION))
+        .contentType(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(AvailableModels.class)
+        .block();
+  }
 }
