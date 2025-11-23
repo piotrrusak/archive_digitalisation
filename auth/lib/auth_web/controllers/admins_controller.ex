666eb066e830000a13a11055b3ab9e7b372cae0d
Ernest <ernest.szlamczyk@swmansion.com>
2025-11-23 13:25:51 +0100

    protected from non-admins setting new admins

--- auth/lib/auth_web/controllers/admins_controller.ex
+++ auth/lib/auth_web/controllers/admins_controller.ex
@@ -33,24 +33,30 @@ defmodule AuthWeb.AdminsController do
   end
 
   def set_admin(conn, %{"email" => email, "is_admin" => admin?}) do
-    case Accounts.get_user_by_email(email) do
-      nil ->
-        conn
-        |> put_status(:not_found)
-        |> json(%{error: "User with given email not found"})
+    if conn.current_user.admin do
+      case Accounts.get_user_by_email(email) do
+        nil ->
+          conn
+          |> put_status(:not_found)
+          |> json(%{error: "User with given email not found"})
 
-      %User{} = user ->
-        new_user = Accounts.set_user_admin(user, admin?)
+        %User{} = user ->
+          new_user = Accounts.set_user_admin(user, admin?)
 
-        conn
-        |> put_status(:ok)
-        |> json(%{
-          message:
-            if(new_user.admin,
-              do: "Successfully set a new admin!",
-              else: "Successfully removed admin!"
-            )
-        })
+          conn
+          |> put_status(:ok)
+          |> json(%{
+            message:
+              if(new_user.admin,
+                do: "Successfully set a new admin!",
+                else: "Successfully removed admin!"
+              )
+          })
+      end
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> json(%{error: "Unauthorized"})
     end
   end
 end
