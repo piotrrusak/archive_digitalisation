a80766f27c2f725e9c461cfbb03d84d00ce70dd7
Ernest <ernest.szlamczyk@swmansion.com>
2025-11-26 20:27:20 +0100

    doc status displays active to avoid confusion

--- frontend/src/components/documents/DocStatus.tsx
+++ frontend/src/components/documents/DocStatus.tsx
@@ -7,8 +7,8 @@ interface DocStatusProps {
 export const DocStatus: FC<DocStatusProps> = () => {
   // TODO: implement if we have time to include status in documents
   return (
-    <span className="inline-flex items-center rounded-full bg-gray-100 px-2 py-0.5 text-xs text-gray-600">
-      Pending
+    <span className="inline-flex items-center rounded-full bg-blue-accent px-2 py-0.5 w-[100px] justify-center text-xs text-gray-text">
+      Active
     </span>
   )
 }
