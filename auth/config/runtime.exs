2877617f8f2b847bf1e3a9c9cadf79788854ea0f
Ernest <ernest.szlamczyk@swmansion.com>
2026-01-04 13:44:57 +0100

    remove certificate verification for the db

--- auth/config/runtime.exs
+++ auth/config/runtime.exs
@@ -44,6 +44,7 @@ if config_env() == :prod do
     ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    ssl_opts: [verify: :verify_none],
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
